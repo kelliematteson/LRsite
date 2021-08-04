407774b2d27064f2c4608e1adddb17d501d231fe
Kellie Matteson <kelliematteson@yahoo.com>
2021-08-03 20:45:06 -0700

    added full bio

---
+++ src/components/AboutSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function AboutSection() {
+
+
+
+
+    return (
+        <p className="aboutText">Leena Rao is a freelance journalist who writes about fascinating women
+        in business, fashion, health, and entrepreneurship. She covers stories
+        for a variety of publications and clients including Detroit-based SEEN
+        Magazine, Marie Claire, Veronica Beard, and ELLE Magazine. Formerly a
+        full-time technology reporter for Fortune Magazine, she wrote
+        investigative and feature articles about companies including Apple,
+        Uber, Google, Amazon and Instagram. She has also written in-depth
+        profiles on female leaders from Alphabet and Google CFO Ruth Porat to
+        Kim Kardashian to Jessica Alba. A mom to two little girls and a
+        goldendoodle, Leena is married to entrepreneur and author, Suneel
+        Gupta. On a perfect day, she does Pilates in the morning, writes in
+        the afternoon, cooks healthy things that taste good in the evening,
+        and reads with her husband and girls at night. The juggle is real.</p>
+    )
+}
